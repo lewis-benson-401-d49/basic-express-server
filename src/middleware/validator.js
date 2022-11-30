82654efcb3dbd4ed779e46983c7e4cc5c6f155b4
Lewis Benson <tm.lewis.benson@gmail.com>
2022-11-29 19:07:33 -0500

    add validation to file

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -2,8 +2,11 @@
 
 
 module.exports = (req, res, next) => {
-  if(req.query.name) next();
-  res.status(500);
-  next('Name is required.');
+  if (req.query.name) {
+    next();
+  } else {
+    res.status(500);
+    next('Name is required.');
+  }
 };
 
